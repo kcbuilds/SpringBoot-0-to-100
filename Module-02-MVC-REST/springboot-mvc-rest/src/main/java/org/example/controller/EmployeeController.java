fcc955a267a22e1cebbc88b7d0dc5c9be4ede605
kcbuilds <karanchaurasiya.dev@gmail.com>
2026-06-07 19:10:58 +0530

    Module 2

---
+++ Module-02-MVC-REST/springboot-mvc-rest/src/main/java/org/example/controller/EmployeeController.java
@@ -0,0 +1,13 @@
+package org.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+    @GetMapping(path = "/getSecretMessage")
+    public String getMySuperSecretMessage() {
+        return "Secret message: #DDFC15242524R";
+    }
+}
